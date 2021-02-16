f80821d6c6e0418be1423c8a02ee9d7917a26c28
Kyllian LOUIS JOSEPH <lj.kyllian@gmail.com>
2021-02-16 09:27:34 +0100

    page acceuil

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>euuuuuuuuuuh</title>
+    <title>acceuil</title>
 </head>
 <body>
-    <?php
-        echo "mdr"
-    ?>
+
 </body>
 </html>
