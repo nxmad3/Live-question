3958d4f943cb4d5f3a49ae111112eddaaf27fa4f
Enzo N'ga <enzo.nga93@gmail.Com>
2021-02-16 16:33:54 +0100

    Mise en place des cases déroulantes + création du css

--- index.php
+++ index.php
@@ -1,34 +1,65 @@
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-    <title>acceuil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Style.css">
 </head>
-<div>
-    <div class="FAQ">
-        <p>FAQ</p>
-    </div>
+<body>
+<ul>
+    <li>
+        <button class="collapsible">Can i upgrade later on?</button>
+        <div class="content">
+            <p>Yes you can.</p>
+        </div>
+    </li>
+    <li>
+        <button class="collapsible">Can i port my data from another provider?</button>
+        <div class="content">
+            <p>Yes you can.</p>
+        </div>
+    </li>
+    <li>
+        <button class="collapsible">Are my food photos stored forever in the cloud</button>
+        <div class="content">
+            <p>Lmao no it's not possible you dumb.</p>
+        </div>
+    </li>
+    <li>
+        <button class="collapsible">Who foots the bill for that?</button>
+        <div class="content">
+            <p>Yo mama aahahah.</p>
+        </div>
+    </li>
+    <li>
+        <button class="collapsible">What's the real cost?</button>
+        <div class="content">
+            <p>21 you stupid.</p>
+        </div>
+    </li>
+    <li>
+        <button class="collapsible">Can my company request a custom plan?</button>
+        <div class="content">
+            <p>My man that's why you are here for.</p>
+        </div>
+    </li>
+</ul>
 
-    <div class="text"
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Vestibulum suscipit at nisi vel tincidunt.
-        Maecenas scelerisque tortor a mauris commodo pulvinar.</p>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
-        integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
-        integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
-        crossorigin="anonymous"></script>
+<script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+</script>
+
 </body>
 </html>
