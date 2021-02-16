5b761b4e3e76ffeeb49dc40c5550adf86043a893
Enzo N'ga <enzo.nga93@gmail.Com>
2021-02-16 10:12:07 +0100

    Mise en page de FAQ (update kiki)

--- index.php
+++ index.php
@@ -9,8 +9,17 @@
 
     <title>acceuil</title>
 </head>
-<body>
+<div>
+<div class="FAQ">
+    <p>FAQ</p>
+</div>
 
+<div class="text"
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Vestibulum suscipit at nisi vel tincidunt.
+    Maecenas scelerisque tortor a mauris commodo pulvinar.</p>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
