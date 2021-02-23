189b500bae28337db512cb551381d7cc9c93523e
Enzo N'ga <enzo.nga93@gmail.Com>
2021-02-23 12:09:22 +0100

    partie fini (taille pas fini)

--- index.php
+++ index.php
@@ -5,45 +5,71 @@
     <link rel="stylesheet" href="Style.css">
 </head>
 <body>
-<ul>
-    <li>
-        <button class="collapsible">Can i upgrade later on?</button>
-        <div class="content">
-            <p>Yes you can.</p>
-        </div>
-    </li>
-    <li>
-        <button class="collapsible">Can i port my data from another provider?</button>
-        <div class="content">
-            <p>Yes you can.</p>
-        </div>
-    </li>
-    <li>
-        <button class="collapsible">Are my food photos stored forever in the cloud</button>
-        <div class="content">
-            <p>Lmao no it's not possible you dumb.</p>
-        </div>
-    </li>
-    <li>
-        <button class="collapsible">Who foots the bill for that?</button>
-        <div class="content">
-            <p>Yo mama aahahah.</p>
-        </div>
-    </li>
-    <li>
-        <button class="collapsible">What's the real cost?</button>
-        <div class="content">
-            <p>21 you stupid.</p>
-        </div>
-    </li>
-    <li>
-        <button class="collapsible">Can my company request a custom plan?</button>
-        <div class="content">
-            <p>My man that's why you are here for.</p>
-        </div>
-    </li>
-</ul>
+<div class="blockfaq">
+    <div class="faq">
+        <p>FAQ</p>
+    </div>
 
+    <div class="ask">
+        <p>Sed ut percepeciatis unde omnis iste natus error sit voluptatem <br/>
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+    </div>
+
+    <ul>
+        <li>
+            <button class="collapsible">Can i upgrade later on?</button>
+            <div class="content">
+                <p>Yes you can.</p>
+            </div>
+        </li>
+        <li>
+            <button class="collapsible">Can i port my data from another provider?</button>
+            <div class="content">
+                <p>Yes you can.</p>
+            </div>
+        </li>
+        <li>
+            <button class="collapsible">Are my food photos stored forever in the cloud</button>
+            <div class="content">
+                <p>Lmao no it's not possible you dumb.</p>
+            </div>
+        </li>
+        <li>
+            <button class="collapsible">Who foots the bill for that?</button>
+            <div class="content">
+                <p>Yo mama aahahah.</p>
+            </div>
+        </li>
+        <li>
+            <button class="collapsible">What's the real cost?</button>
+            <div class="content">
+                <p>21 you stupid.</p>
+            </div>
+        </li>
+        <li>
+            <button class="collapsible">Can my company request a custom plan?</button>
+            <div class="content">
+                <p>My man that's why you are here for.</p>
+            </div>
+        </li>
+    </ul>
+
+    <div class="ques"
+    <p>Still have unanswered questions? <span style="color:rgba(209,1,113,255)">Get in touch</span></p>
+</div>
+
+
+<div class="footer">
+    <div class="vale">
+    </div>
+    <div class="infooter">
+        <p>©2019 Page protected by reCAPTCHA and subject to Google's <span style="color: white; font-weight: bold"> Privacy policy </span>
+            and
+            <span style="color: white; font-weight: bold"> Term of Services </span></p>
+        <img src="links.jpg">
+    </div>
+</div>
+</div>
 <script>
     var coll = document.getElementsByClassName("collapsible");
     var i;
