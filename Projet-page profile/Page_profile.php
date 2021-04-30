54866b64d5945aa5c023e21b12f29f0ba1454961
SGNoxx <79085868+SGNoxx@users.noreply.github.com>
2021-04-30 18:27:48 +0200

    Version fini

--- Projet-page profile/Page_profile.php
+++ Projet-page profile/Page_profile.php
@@ -1,21 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Profile</title>
-	<link rel="stylesheet" type="text/css" href="Page_profile.css">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" rel="stylesheet">
-</head>
-<body>
+<?php
+    require("header.php");
+    require("./db/db.php");
+    $co = connexionBdd();
+?>
 <div class="row py-5 px-4">
     <div class="col-md-5 mx-auto">
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="photo.png" alt="..." width="130" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Editer le profile</a></div>
+                    <div class="profile mr-3"><img src="photo.png" alt="..." width="130" class="rounded mb-2 img-thumbnail"><a data-toggle="modal" data-target="#exampleModal" class="btn btn-outline-dark btn-sm btn-block">Editer le profile</a></div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form name="insertion" action="Page_Profile.php" method="POST">
+<table width="500" border="0" align="center" cellpadding="6" cellspacing="0" bgcolor="">
+<tr>
+<td><FONT COLOR="">Nouveau mot de passe :<b style="color: #990000;" >*</b></color></td>
+<td><input type="password" name="pass" placeholder="Ancien mot de passe"></td></tr>
+<tr>
+<td><FONT COLOR="">Nouveau pseudo :<b style="color: #990000;">*</b></color></td>
+<td><input type="text" name="pseudo" placeholder=<?php echo($donnees["pseudoUtilisateur"])?>></td></tr>
+<tr>
+<td><FONT COLOR="">Nouveau mail:</color></td>
+<td><input type="email" name="email" placeholder="Ancien mail"></td></tr>
+
+      </div></td>
+</table>
+<div class="modal-footer">
+        <button type="submit" class="btn btn-primary" name="confirm" value="1">Save changes</button>
+      </div>
+             </form>
+      </div>
+      
+    </div>
+  </div>
+</div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">"Le nom"</h4><i class="fas fa-map-marker-alt mr-2"></i>
+                        <h4  class="text-dark"><?php echo $_SESSION["pseudoUtilisateur"] ?></h4><i class="fas fa-map-marker-alt mr-2"></i>
                     </div>
                 </div>
             </div>
@@ -35,14 +64,56 @@
             <div class="px-4 py-3">
                 <h5 class="mb-0">Les questions posées</h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">Question 1</p>
-                    <p class="font-italic mb-0">Question 2</p>
-                    <p class="font-italic mb-0">Question 3</p>
+                    <?php
+                    $idUtilisateur = $_SESSION['idUtilisateur'];
+                    $questions = $co->query("SELECT * FROM question INNER JOIN categorie on question.cat_idQuestion = categorie.idCategorie INNER JOIN utilisateurs on question.auteur_idQustion = utilisateurs.idUtilisateurs WHERE auteur_idQustion=$idUtilisateur");
+                    while ($donnees = $questions->fetch()){
+                    ?>
+                    <?php
+                    ?>
+                    <strong>
+                        <?php
+                        echo($donnees["pseudoUtilisateur"]);
+                        ?>
+                    </strong>
+                    <?php
+                    echo("&nbsp Question: ");
+                    echo($donnees["titreQuestion"]);
+                    echo("<br> Categorie: ");
+                    echo($donnees["nomCategorie"]);
+                    echo("<br> Sujet: ");
+                    echo($donnees["sujetQuestion"]);
+                    ?>
+                    <i>
+                        <?php
+                        echo("<br> le: ");
+                        echo($donnees["date_creaQuestion"]);
+                        ?>
+                    </i>
+                </div>
+            </div>
+            <?php
+            }
+            $questions->closeCursor();
+            ?>
                 </div>
             </div>
             
         </div>
     </div>
+    <?php
+    var_dump($_SESSION);
+    var_dump($_POST);
+        if(isset($_POST['confirm'])){
+        $pass = $_POST['pass'];
+        $pseudo = $_POST['pseudo'];
+        $email = $_POST['email'];
+
+        $requete = $co->query("UPDATE `utilisateurs` SET `pseudoUtilisateur` = '" . $pseudo . "', `emailUtilisateur` = '" . $email . "', `mot_de_passeUtilisateur` = '" . $pass . "' WHERE `idUtilisateurs` =" . $idUtilisateur);
+        echo("<meta http-equiv='refresh' content='0.5'>");        
+        }
+        
+    ?>
 </div>
 </body>
 </html>
