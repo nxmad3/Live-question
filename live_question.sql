f05d58127ebdc0f651bafb70cdd33123c6b2989c
Yuki60 <79085918+Yuki60@users.noreply.github.com>
2021-04-30 18:48:26 +0200

    Add files via upload

--- live_question.sql
+++ live_question.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Ven 12 Mars 2021 à 22:29
--- Version du serveur :  5.7.11
--- Version de PHP :  7.0.3
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 30 avr. 2021 à 16:38
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,8 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `live_question`
+-- Base de données : `live_question`
 --
+CREATE DATABASE IF NOT EXISTS `live_question` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `live_question`;
 
 -- --------------------------------------------------------
 
@@ -26,10 +29,20 @@ SET time_zone = "+00:00";
 -- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
-  `idContact` int(11) NOT NULL,
-  `nomContact` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `idCategorie` int NOT NULL AUTO_INCREMENT,
+  `nomCategorie` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`idCategorie`, `nomCategorie`) VALUES
+(8, 'anime'),
+(7, 'voiture');
 
 -- --------------------------------------------------------
 
@@ -37,13 +50,30 @@ CREATE TABLE `categorie` (
 -- Structure de la table `question`
 --
 
-CREATE TABLE `question` (
-  `idQuestion` int(11) NOT NULL,
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `idQuestion` int NOT NULL AUTO_INCREMENT,
+  `auteur_idQustion` int DEFAULT NULL,
   `titreQuestion` varchar(45) DEFAULT NULL,
-  `cat_idQuestion` int(11) DEFAULT NULL,
-  `auteur_idQustion` int(11) DEFAULT NULL,
-  `date_creaQuestion` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `sujetQuestion` varchar(255) NOT NULL,
+  `cat_idQuestion` int DEFAULT NULL,
+  `date_creaQuestion` date DEFAULT NULL,
+  PRIMARY KEY (`idQuestion`),
+  KEY `fk_cat_id` (`cat_idQuestion`),
+  KEY `fk_aut_id` (`auteur_idQustion`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `question`
+--
+
+INSERT INTO `question` (`idQuestion`, `auteur_idQustion`, `titreQuestion`, `sujetQuestion`, `cat_idQuestion`, `date_creaQuestion`) VALUES
+(1, 1, 'j\'aime voirture', '', 1, '2021-04-20'),
+(2, 1, 'j\'aime voirture', '', 1, '2021-04-20'),
+(3, NULL, 'test', 'reussi', 8, NULL),
+(4, NULL, 'test', 'reussi avec la date', 8, '2021-04-20'),
+(5, 1, 'test avec auteur ', 'sa marche ?', 7, '2021-04-23'),
+(6, 1, 'test avec auteur ', 'sa marche ?', 7, '2021-04-23');
 
 -- --------------------------------------------------------
 
@@ -51,12 +81,34 @@ CREATE TABLE `question` (
 -- Structure de la table `reponse`
 --
 
-CREATE TABLE `reponse` (
-  `user_idReponse` int(11) DEFAULT NULL,
-  `question_idReponse` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `reponse`;
+CREATE TABLE IF NOT EXISTS `reponse` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_idReponse` int NOT NULL,
+  `question_idReponse` int NOT NULL,
   `dateReponse` datetime DEFAULT NULL,
-  `rep_questReponse` text
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `rep_questReponse` text,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_idReponse` (`user_idReponse`,`question_idReponse`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reponse`
+--
+
+INSERT INTO `reponse` (`id`, `user_idReponse`, `question_idReponse`, `dateReponse`, `rep_questReponse`) VALUES
+(1, 1, 0, '2021-04-30 00:00:00', 'aty,ytgfd'),
+(3, 1, 0, '2021-04-30 00:00:00', 'aty,ytgfd'),
+(4, 1, 0, '2021-04-30 00:00:00', 'test'),
+(5, 1, 0, '2021-04-30 00:00:00', 'test'),
+(6, 1, 0, '2021-04-30 00:00:00', 'test'),
+(7, 1, 0, '2021-04-30 00:00:00', 'test'),
+(8, 1, 5, '2021-04-30 00:00:00', 'test'),
+(9, 1, 5, '2021-04-30 00:00:00', 'test'),
+(10, 1, 6, '2021-04-30 00:00:00', 'toto'),
+(11, 1, 6, '2021-04-30 00:00:00', 'toto'),
+(12, 1, 6, '2021-04-30 00:00:00', 'toto'),
+(13, 1, 6, '2021-04-30 00:00:00', 'toto');
 
 -- --------------------------------------------------------
 
@@ -64,67 +116,28 @@ CREATE TABLE `reponse` (
 -- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE `utilisateurs` (
-  `idUtilisateurs` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `idUtilisateurs` int NOT NULL AUTO_INCREMENT,
   `pseudoUtilisateur` varchar(255) DEFAULT NULL,
   `emailUtilisateur` varchar(255) DEFAULT NULL,
   `mot_de_passeUtilisateur` varchar(255) DEFAULT NULL,
   `avatarUtilisateur` varchar(255) DEFAULT NULL,
-  `genreUtilisateur` char(1) DEFAULT NULL,
+  `genreUtilisateur` varchar(255) DEFAULT NULL,
   `date_inscription` datetime DEFAULT NULL,
-  `roleUtilisateur` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `roleUtilisateur` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idUtilisateurs`)
+) ENGINE=MyISAM AUTO_INCREMENT=327 DEFAULT CHARSET=latin1;
 
 --
--- Index pour les tables exportées
+-- Déchargement des données de la table `utilisateurs`
 --
 
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`idContact`);
+INSERT INTO `utilisateurs` (`idUtilisateurs`, `pseudoUtilisateur`, `emailUtilisateur`, `mot_de_passeUtilisateur`, `avatarUtilisateur`, `genreUtilisateur`, `date_inscription`, `roleUtilisateur`) VALUES
+(1, 'test', 'test@test.fr', 'test', NULL, 'test', NULL, NULL),
+(326, 'test2', 'azerty@test.fr', 'test', NULL, 'mdr', NULL, NULL);
+COMMIT;
 
---
--- Index pour la table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`idQuestion`),
-  ADD KEY `fk_cat_id` (`cat_idQuestion`),
-  ADD KEY `fk_aut_id` (`auteur_idQustion`);
-
---
--- Index pour la table `reponse`
---
-ALTER TABLE `reponse`
-  ADD KEY `fk_user_id` (`user_idReponse`),
-  ADD KEY `fk_quest_id` (`question_idReponse`);
-
---
--- Index pour la table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`idUtilisateurs`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `idContact` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `question`
---
-ALTER TABLE `question`
-  MODIFY `idQuestion` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  MODIFY `idUtilisateurs` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ test.php
@@ -0,0 +1,99 @@
+<?php
+require("header.php")
+?>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#Acceuil">Accueil<span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#questions">Questions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Profil">Profil</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <button type="button">Deconexion</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="all">
+    <div class="nindo">
+        <h1> Les questions </h1>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <?php
+            require("./db/db.php");
+            $co = connexionBdd();
+            $questions = $co->query("SELECT * FROM question INNER JOIN categorie on question.cat_idQuestion = categorie.idCategorie INNER JOIN utilisateurs on question.auteur_idQustion = utilisateurs.idUtilisateurs");
+
+            ?>
+            <form action="test.php" role="form" method="POST">
+
+                <?php
+                while ($donnees = $questions->fetch()){
+                    ?>
+                    <div class="list-question">
+                        <span class="pseudo"><?php echo $donnees["pseudoUtilisateur"] ?></span>
+                        <span class="titreQuestion">Question: <?php echo $donnees["titreQuestion"] ?></span>
+                        <span class="nomCategorie">Categorie: <?php echo $donnees["nomCategorie"] ?></span>
+                        <span class="sujetQuestion">Sujet: <?php echo $donnees["sujetQuestion"] ?></span>
+                        <span class="date_creaQuestion">le: <?php echo $donnees["date_creaQuestion"] ?></span>
+                        <br>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Réponse<span class="caret"></span></button>
+                            <ul class="dropdown-menu" role="menu">
+                                <div class="form-group">
+                                    <label for="reponse" style="float:left;">reponse</label>
+                                    <input type="hidden" name="idQuestion" value="<?php echo $donnees['idQuestion']?>">
+                                    <input id="reponse" class="form-control" type="text" placeholder="Votre reponse" rows="7" name="reponse"></input>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+                <button type="submit" style="text-align: center;" class="btn btn-success">Envoyer</button>
+            </form>
+        </div>
+    </div>
+</div>
+<?php
+if (isset($_POST["reponse"])){
+    try {
+        // prepare sql and bind parameters
+        $questionId = $_POST['idQuestion'];
+        $reponse = $_POST['reponse'];
+        $idUtilisateur=$_SESSION["idUtilisateur"];
+        $date = date("Y-m-d");
+        $stmt = $co->prepare("INSERT INTO `reponse`(`question_idReponse`,`user_idReponse`, `dateReponse`, `rep_questReponse`) VALUES (:question_idReponse ,:user_idReponse,:dateReponse,:rep_questReponse)");
+        $stmt->bindParam(':user_idReponse', $idUtilisateur);
+        $stmt->bindParam(':question_idReponse', $questionId);
+        $stmt->bindParam(':dateReponse', $date);
+        $stmt->bindParam(':rep_questReponse', $reponse);
+
+        $stmt->execute();
+
+        echo "New records created successfully";
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+$conn = null;
+$questions->closeCursor();
+?>
+</body>
+</html>
